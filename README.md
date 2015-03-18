b641bf51278e729debec468bb0f15f0e58da003e
Benjamin Eidelman <beneidel@gmail.com>
2015-03-18 15:46:51 -0300

    touch support

---
+++ README.md
@@ -0,0 +1,6 @@
+Color Sampler
+===========
+
+UI experiment showing a color selector inspired in a material color sampler, mouse and touch friendly.
+
+[live demo](http://benjamine.github.io/color-sampler/)
--- color-sampler.js
+++ color-sampler.js
@@ -3,6 +3,10 @@ var sampler = document.querySelector('.color-sampler');
 var items = document.querySelectorAll('.color-sampler li');
 var img =  document.querySelector('.selected-image img');
 var colorName = document.querySelector('.color-name');
+sampler.setAttribute('title', 'click to expand');
+var isTouchDevice = 'ontouchstart' in document.documentElement;
+
+document.querySelector('body').classList.add(isTouchDevice ? 'touch-device' : 'mouse-device');
 
 Array.prototype.forEach.call(items, function(item) {
   var color = item.innerText.toLowerCase().replace(/ /g, '-');
@@ -22,37 +26,120 @@ document.body.addEventListener('click', function(e) {
     return;
   }
   sampler.classList.remove('expanded');
+  sampler.setAttribute('title', 'click to expand');
   var selectedItem = document.querySelector('.color-sampler .selected');
   showColor(selectedItem);
 });
 
-sampler.addEventListener('click', function(e) {
+sampler.addEventListener('dragstart', function(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation();
+  }
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
+});
+
+sampler.addEventListener('mousemove', function(e) {
   if (e.target.nodeName !== 'LI') {
     return;
   }
   if (!sampler.classList.contains('expanded')) {
-    sampler.classList.add('expanded');
     return;
   }
-  document.querySelector('.color-sampler .selected').classList.remove('selected');
-  e.target.classList.add('selected');
-  sampler.classList.remove('expanded');
-
-  showColor(e.target);
+  window.getSelection().removeAllRanges();
 });
 
-var over = false;
+var expandTime;
 
-sampler.addEventListener('mouseover', function(e) {
-  over = true;
+function elementUnderPointer(e) {
+
+  if (event.touches && event.touches[0]) {
+    var x = e.pageX || event.touches[0].pageX;
+    var y = e.pageY || event.touches[0].pageY;
+    if (x && y) {
+      x = x - (window.pageXOffset || 0);
+      y = y - (window.pageYOffset || 0);
+      var item = document.elementFromPoint(x, y);
+      return document.elementFromPoint(x, y);
+    }
+  }
+  return e.target;
+}
+
+function touchStart(e) {
   if (e.target.nodeName !== 'LI') {
     return;
   }
-  showColor(e.target);
-});
+  if (selectionTime &&
+    new Date().getTime() - selectionTime.getTime() < 200) {
+    return;
+  }
+  if (itemOver) {
+    itemOver.classList.remove('hover');
+  }
+  itemOver = e.target;
+  itemOver.classList.add('hover');
 
-sampler.addEventListener('mouseleave', function(e) {
+  if (!sampler.classList.contains('expanded')) {
+    sampler.classList.add('expanded');
+    sampler.setAttribute('title', '');
+    expandTime = new Date();
+    return;
+  }
+}
+
+var selectionTime;
+
+function touchEnd(e) {
+  var target = itemOver || elementUnderPointer(e);
+  if (itemOver) {
+    itemOver.classList.remove('hover');
+    itemOver = null;
+  }
+
+  if (target.nodeName !== 'LI') {
+    return;
+  }
+  if (!sampler.classList.contains('expanded')) {
+    return;
+  }
+  if (new Date().getTime() - expandTime.getTime() < 300) {
+    return;
+  }
+  document.querySelector('.color-sampler .selected').classList.remove('selected');
+  target.classList.add('selected');
+  selectionTime = new Date();
+  sampler.classList.remove('expanded');
+  sampler.setAttribute('title', 'click to expand');
+
+  showColor(target);
+}
+
+var over = false;
+var itemOver;
+
+function touchMove(e) {
+  e.preventDefault();
+  over = true;
+  var target = elementUnderPointer(e);
+  if (target.nodeName !== 'LI') {
+    return;
+  }
+  if (itemOver) {
+    itemOver.classList.remove('hover');
+  }
+  itemOver = target;
+  itemOver.classList.add('hover');
+  showColor(target);
+}
+
+function touchLeave(e) {
   over = false;
+  if (itemOver) {
+    itemOver.classList.remove('hover');
+    itemOver = null;
+  }
   setTimeout(function() {
     if (over) {
       return;
@@ -60,7 +147,19 @@ sampler.addEventListener('mouseleave', function(e) {
     var selectedItem = document.querySelector('.color-sampler .selected');
     showColor(selectedItem);
   }, 1000);
-});
+}
+
+sampler.addEventListener('mousedown', touchStart);
+sampler.addEventListener('touchstart', touchStart);
+
+sampler.addEventListener('mouseup', touchEnd);
+sampler.addEventListener('touchend', touchEnd);
+
+sampler.addEventListener('mouseover', touchMove);
+sampler.addEventListener('touchmove', touchMove);
+
+sampler.addEventListener('mouseleave', touchLeave);
+sampler.addEventListener('touchleave', touchLeave);
 
 function showColor(item) {
   var color = item.getAttribute('data-color');
